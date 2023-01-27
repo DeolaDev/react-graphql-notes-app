1929fef12eaf3c7426e1890f463a46146c983626
DeolaDev <deoladeveloper@gmail.com>
2023-01-26 22:17:47 -0500

    Updated App.jsx

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactgraphqlnotesapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "reactgraphqlnotesapp5fff544c": {
       "AppClientID": "string",
